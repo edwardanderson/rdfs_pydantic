afc94459d9b4422d4b68ece8a3fe81c72874211f
Edward Anderson <anderson.edw@gmail.com>
2026-01-26 21:04:27 +0000

    rm

--- tmp_pkg/la/DigitalService.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-from .._base import OptimizedBaseModel
-
-class DigitalService(OptimizedBaseModel):
-    """Digital Service <https://linked.art/ns/terms/DigitalService>."""
-    ...
